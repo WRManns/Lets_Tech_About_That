104231d4f2354849689c73f5d7f36824f16e2071
Ryan Manns <w.r.manns@outlook.com>
2021-07-28 18:18:36 -0400

    comment.js being called, but api comment route is not

--- Develop/controllers/api/commentRoutes.js
+++ Develop/controllers/api/commentRoutes.js
@@ -3,6 +3,7 @@ const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 
+
 router.get('/', (req, res) => {
     Comment.findAll({})
         .then(commentData => res.json(commentData))
@@ -12,30 +13,47 @@ router.get('/', (req, res) => {
         })
 });
 
-// router.get('/:id', (req, res) => {
-//     Comment.findAll({
-//             where: {
-//                 id: req.params.id
-//             }
-//         })
-//         .then(commentData => res.json(commentData))
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         })
-// });
-
-router.post('/', withAuth, (req, res) => {
-    if (req.session) {
-      Comment.create({
-        comment_text: req.body.comment_text,
-        blog_id: req.body.blog_id,
-        user_id: req.session.user_id,
-      }).then(dbCommentData => res.json(dbCommentData))
+router.get('/:id', (req, res) => {
+    Comment.findAll({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(commentData => res.json(commentData))
         .catch(err => {
-          res.status(400).json(err);
-        });
-    }
-  });
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    
+    const newComment = await Comment.create(
+      {
+      comment_text: req.body.comment_text,
+      blog_id: req.body.blod_id,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newComment);
+    console.log(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// router.post('/', withAuth, (req, res) => {
+//     if (req.session) {
+//       Comment.create({
+//         comment_text: req.body.comment_text,
+//         blog_id: req.body.blog_id,
+//         user_id: req.session.user_id,
+//       }).then(commentData => res.json(commentData))
+//         .catch(err => {
+//           res.status(400).json(err);
+//         });
+//     }
+//   });
 
 module.exports = router;
--- Develop/public/js/comment.js
+++ Develop/public/js/comment.js
@@ -2,25 +2,29 @@
     event.preventDefault();
   
     const comment_text = document.querySelector('#comment-text').value.trim();
-  
-    if (comment_text) {
+
+    const blog_id = window.location.toString().split("/")[
+      window.location.toString().split("/").length - 1]
+
+    if (comment_text && blog_id) {
       const response = await fetch(`/api/comments`, {
         method: 'POST',
-        body: JSON.stringify({ comment_text }),
+        body: JSON.stringify({ blog_id, comment_text }),
         headers: {
           'Content-Type': 'application/json',
         },
       });
   
       if (response.ok) {
-        document.location.replace `/api/post/${id}`;
+        console.log(comment_text);
+        document.location.replace`/post/${id}`;
       } else {
         alert('Failed to create comment');
       }
     }
   };
   
-  
+  // $('.new-comment-form').on('submit', newCommentHandler());
   document
     .querySelector('.new-comment-form')
     .addEventListener('submit', newCommentHandler);

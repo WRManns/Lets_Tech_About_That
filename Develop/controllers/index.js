7026778742fab51de1f74903ad3d2f1ea0ac3563
Ryan Manns <w.r.manns@outlook.com>
2021-07-28 19:46:16 -0400

    still truggling with the edit route

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const editorRoutes = require('./editorRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/edit',editorRoutes);
 
 module.exports = router;

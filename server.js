e5779c94b60503a01a8118af58ea1df304673ba7
Ryan Manns <w.r.manns@outlook.com>
2021-07-29 15:06:06 -0400

    triple checked dotenv install

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',

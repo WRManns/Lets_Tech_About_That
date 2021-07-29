485168500c9ccbe2dab538d76b1d39aef8c0d716
Ryan Manns <w.r.manns@outlook.com>
2021-07-29 14:40:00 -0400

    removed unworking edit code

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,8 +3,12 @@ const userRoutes = require('./userRoutes');
 const blogPostRoutes = require('./blogPostRoutes');
 const commentRoutes = require('./commentRoutes');
 
+
+
 router.use('/users', userRoutes);
 router.use('/posts', blogPostRoutes);
 router.use('/comments', commentRoutes);
+
+
 
 module.exports = router;
